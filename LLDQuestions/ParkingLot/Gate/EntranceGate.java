2183d7971f136769f4416174a1e70ca86d761a22
Anusatya <anusatya.choudhary@gmail.com>
2026-01-12 22:21:19 +0530

    Add EntranceGate class: Implemented functionality for vehicle entry into the parking lot, including ticket allocation and handling of no available spots.

---
+++ LLDQuestions/ParkingLot/Gate/EntranceGate.java
@@ -0,0 +1,9 @@
+public class EntranceGate {
+    public Ticket enter(ParkingBuilding parkingBuilding, Vehicle vehicle){
+        Ticket ticket = parkingBuilding.allocate(vehicle);
+        if(ticket == null){
+            throw new IllegalStateException("No parking spots available");
+        }
+        return ticket;
+    }
+}
