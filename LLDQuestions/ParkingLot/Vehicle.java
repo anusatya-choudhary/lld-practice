27683992725562a8d0c46dca6b30d0997f046f3e
Anusatya <anusatya.choudhary@gmail.com>
2026-01-12 17:57:26 +0530

    Add ParkingLot classes: Introduced ParkingSpot, Vehicle, and VehicleType classes to manage parking spot status and vehicle details.

---
+++ LLDQuestions/ParkingLot/Vehicle.java
@@ -0,0 +1,17 @@
+public class Vehicle {
+    private String vehicleNumber;
+    private VehicleType vehicleType;
+
+    public Vehicle(String vehicleNumber, VehicleType vehicleType) {
+        this.vehicleNumber = vehicleNumber;
+        this.vehicleType = vehicleType;
+    }
+
+    public String getVehicleNumber() {
+        return vehicleNumber;
+    }
+    
+    public VehicleType getVehicleType() {
+        return vehicleType;
+    }
+}
