6272a502c4b1d01c29b5faa559f816a467009b5e
Anusatya <anusatya.choudhary@gmail.com>
2026-02-07 16:02:39 +0530

    Add Piece class for Tic Tac Toe: Introduced Piece and PieceType classes to represent game pieces (X and O) and encapsulate their behavior in the Tic Tac Toe model.

---
+++ LLDQuestions/TicTacToe/model/Piece.java
@@ -0,0 +1,18 @@
+package tictactoe.model;
+
+public enum PieceType {
+    X, O;
+}
+
+
+public class Piece {
+    private final PieceType pieceType;
+
+    public Piece(PieceType pieceType) {
+        this.pieceType = pieceType;
+    }
+
+    public PieceType getPieceType() {
+        return pieceType;
+    }
+}
